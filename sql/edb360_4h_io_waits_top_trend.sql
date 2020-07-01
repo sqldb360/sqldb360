5347ae114f2968042da41e75ec373e77d44a6d3c
David Kurtz <info@go-faster.co.uk>
2020-07-01 15:23:28 +0100

    Update edb360_4h_io_waits_top_trend.sql
    Corrections to section 4h
    1. removed unnecessary wait_class_id from analytic partition by clause.  It is already grouped by event_id.
    2 The trend is the cumulative averge of equally weighted averages.  I changed it to a cumulative average waited by number of events counted instead
    3. removed group by instance, and sum per instance in final query joined to snapshots that will aggregate latency per RAC instance producing incorrect results.

--- sql/edb360_4h_io_waits_top_trend.sql
+++ sql/edb360_4h_io_waits_top_trend.sql
@@ -235,6 +235,10 @@ DEF tit_13 = '';
 DEF tit_14 = '';
 DEF tit_15 = '';
 
+REM dmk 1.7.2020 - removed wait_class_id from analytic partition by clause
+REM switched to cumulative average waited by number of events counted instead of average of equally weighted averages, 
+REM removed group by instance, and sum per instance in final query.
+
 BEGIN
   :sql_text_backup := q'[
 WITH
@@ -244,7 +248,7 @@ SELECT /*+ &&sq_fact_hints. */ /* &&section_id..&&report_sequence. */
        dbid,
        instance_number,
        wait_time_milli,
-       wait_count - LAG(wait_count) OVER (PARTITION BY dbid, instance_number, event_id, wait_class_id, wait_time_milli ORDER BY snap_id) wait_count_this_snap
+       wait_count - LAG(wait_count) OVER (PARTITION BY dbid, instance_number, event_id, wait_time_milli ORDER BY snap_id) snap_wait_count
   FROM &&awr_object_prefix.event_histogram
  WHERE snap_id BETWEEN &&minimum_snap_id. AND &&maximum_snap_id.
    AND dbid = &&edb360_dbid.
@@ -254,23 +258,21 @@ average AS (
 SELECT /*+ &&sq_fact_hints. */ /* &&section_id..&&report_sequence. */
        snap_id,
        dbid,
-       instance_number,
-       SUM((CASE wait_time_milli WHEN 1 THEN 0.50 ELSE 0.75 END) * wait_time_milli * wait_count_this_snap)/SUM(wait_count_this_snap) avg_wait_time_milli
+       SUM((CASE wait_time_milli WHEN 1 THEN 0.50 ELSE 0.75 END) * wait_time_milli * snap_wait_count) snap_wait_time_milli,
+       SUM(snap_wait_count) snap_wait_count
   FROM histogram
  WHERE wait_count_this_snap >= 0
  GROUP BY
        snap_id,
-       dbid,
-       instance_number
-HAVING SUM(wait_count_this_snap) > 0
+       dbid
+--HAVING SUM(wait_count_this_snap) > 0
 ),
 average_average AS (
 SELECT /*+ &&sq_fact_hints. */ /* &&section_id..&&report_sequence. */
        a.snap_id,
        a.dbid,
-       a.instance_number,
-       MIN(a.avg_wait_time_milli) avg_wait_time_milli,
-       AVG(b.avg_wait_time_milli) avg_avg_wait_time_milli
+       MIN(a.snap_wait_time_milli/NULLIF(a.snap_wait_count,0)) avg_wait_time_milli,
+       SUM(b.snap_wait_time_milli)/NULLIF(SUM(b.snap_wait_count),0) avg_avg_wait_time_milli
   FROM average a,
        average b
  WHERE b.snap_id         <= a.snap_id
@@ -278,27 +280,34 @@ SELECT /*+ &&sq_fact_hints. */ /* &&section_id..&&report_sequence. */
    AND b.instance_number  = a.instance_number
  GROUP BY
        a.snap_id,
-       a.dbid,
-       a.instance_number
+       a.dbid
 ),
-per_inst AS (
+s AS ( /*min begin/end times for each snap across all instances*/
+SELECT dbid, snap_id,
+       MIN(begin_interval_time) begin_interval_time,
+       MIN(end_interval_time) end_interval_time
+  FROM dba_hist_snapshot s
+ WHERE snap_id BETWEEN &&begin_snap AND &&end_snap
+   AND dbid = &&dbid
+GROUP BY dbid, snap_id
+), 
+per_snap AS (
 SELECT /*+ &&sq_fact_hints. */ /* &&section_id..&&report_sequence. */
        h.snap_id,
        s.begin_interval_time,
        s.end_interval_time,
        h.avg_wait_time_milli,
        h.avg_avg_wait_time_milli
-  FROM average_average   h,
-       &&awr_object_prefix.snapshot s
+  FROM average_average h,
+       s
  WHERE s.snap_id         = h.snap_id
    AND s.dbid            = h.dbid
-   AND s.instance_number = h.instance_number
 )
 SELECT snap_id,
-       TO_CHAR(MIN(begin_interval_time), 'YYYY-MM-DD HH24:MI:SS') begin_time,
-       TO_CHAR(MIN(end_interval_time), 'YYYY-MM-DD HH24:MI:SS') end_time,
-       ROUND(SUM(avg_wait_time_milli), 3) avg_latency_ms,
-       ROUND(SUM(avg_avg_wait_time_milli), 3) latency_trend_ms,
+       TO_CHAR(begin_interval_time, 'YYYY-MM-DD HH24:MI:SS') begin_time,
+       TO_CHAR(end_interval_time, 'YYYY-MM-DD HH24:MI:SS') end_time,
+       ROUND(avg_wait_time_milli, 3) avg_latency_ms,
+       ROUND(avg_avg_wait_time_milli, 3) latency_trend_ms
        0 dummy_03,
        0 dummy_04,
        0 dummy_05,
@@ -312,9 +321,7 @@ SELECT snap_id,
        0 dummy_13,
        0 dummy_14,
        0 dummy_15
-  FROM per_inst
- GROUP BY
-       snap_id
+  FROM per_snap
  ORDER BY
        snap_id
 ]';
@@ -324,168 +331,168 @@ END;
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_01. "&&event_name_01." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_01.'') AND event_name = TRIM(''&&event_name_01.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_02. "&&event_name_02." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_02.'') AND event_name = TRIM(''&&event_name_02.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_03. "&&event_name_03." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_03.'') AND event_name = TRIM(''&&event_name_03.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_04. "&&event_name_04." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_04.'') AND event_name = TRIM(''&&event_name_04.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_05. "&&event_name_05." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_05.'') AND event_name = TRIM(''&&event_name_05.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_06. "&&event_name_06." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_06.'') AND event_name = TRIM(''&&event_name_06.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_07. "&&event_name_07." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_07.'') AND event_name = TRIM(''&&event_name_07.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_08. "&&event_name_08." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_08.'') AND event_name = TRIM(''&&event_name_08.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_09. "&&event_name_09." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_09.'') AND event_name = TRIM(''&&event_name_09.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_10. "&&event_name_10." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_10.'') AND event_name = TRIM(''&&event_name_10.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_11. "&&event_name_11." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_11.'') AND event_name = TRIM(''&&event_name_11.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_12. "&&event_name_12." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_12.'') AND event_name = TRIM(''&&event_name_12.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_13. "&&event_name_13." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_13.'') AND event_name = TRIM(''&&event_name_13.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_14. "&&event_name_14." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_14.'') AND event_name = TRIM(''&&event_name_14.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_15. "&&event_name_15." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_15.'') AND event_name = TRIM(''&&event_name_15.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_16. "&&event_name_16." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_16.'') AND event_name = TRIM(''&&event_name_16.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_17. "&&event_name_17." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_17.'') AND event_name = TRIM(''&&event_name_17.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_18. "&&event_name_18." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_18.'') AND event_name = TRIM(''&&event_name_18.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_19. "&&event_name_19." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_19.'') AND event_name = TRIM(''&&event_name_19.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_20. "&&event_name_20." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_20.'') AND event_name = TRIM(''&&event_name_20.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_21. "&&event_name_21." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_21.'') AND event_name = TRIM(''&&event_name_21.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_22. "&&event_name_22." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_22.'') AND event_name = TRIM(''&&event_name_22.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_23. "&&event_name_23." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_23.'') AND event_name = TRIM(''&&event_name_23.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
 
 DEF skip_lch = '';
 DEF title = '&&wait_class_24. "&&event_name_24." Average Latency';
-DEF abstract = 'Trend is computed as average of "Average" so far. Thus "Trend" at time T[N] is average of "Average" between time T[0] and T[N] where N > 0.<br />'
+DEF abstract = 'Trend is computed as Cumulative Weighted Average so far, weighted by the number of waits in each AWR snapshot.<br />'
 DEF vaxis = 'Average Latency and Trend in milliseconds';
 EXEC :sql_text := REPLACE(:sql_text_backup, '@filter_predicate@', 'wait_class = TRIM(''&&wait_class_24.'') AND event_name = TRIM(''&&event_name_24.'')');
 @@&&skip_diagnostics.edb360_9a_pre_one.sql
